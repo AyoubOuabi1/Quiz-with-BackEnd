ccce196c538d519cbffe571cad018ae511b97a83
Ayoub Ouabi <ayoub.ouabi06@gmail.com>
2023-01-02 11:44:58 +0100

    add login page,user get user data and update MaxScore

---
+++ Controller/user.php
@@ -0,0 +1,106 @@
+<?php
+spl_autoload_register(function($className) {
+    $file = '../Classes/'.$className.'.php';
+    require $file;
+});
+if($_POST['functionName']=="updateUser"){
+    updateUser();
+}else if ($_POST['functionName']=="checkUser"){
+    checkUser();
+}else if ($_POST['functionName']=="maxScore"){
+    getMaxScore();
+}
+function checkUser(){
+    session_start();
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    if(User::checkUser($username,$password)){
+
+        header('Location: http://localhost/QuizBackEnd/quiz.php');
+    }else {
+        echo 'Please enter';
+    }
+}
+function updateUser(){
+    session_start();
+    $addressIp=$_SERVER['REMOTE_ADDR'];
+    $user_agent = $_SERVER['HTTP_USER_AGENT'];
+    $score = $_POST['SCORE'];
+    $user=new User($_SESSION['userData']["id"],$score,getOS($user_agent),getBrowser($user_agent),$addressIp);
+     if($user->updateUser()){
+        echo 'User updated successfully';
+
+    }else {
+        echo 'User update failed';
+    }
+}
+
+function getMaxScore(){
+    session_start();
+    $s= User::getMaxScore($_SESSION['userData']['id']);
+    echo $s['score'];
+}
+function getBrowser($userbrowsername) {
+
+
+    $browser="";
+    $browser_array  = array(
+        '/msie/i'       =>  'Internet Explorer',
+        '/firefox/i'    =>  'Firefox',
+        '/safari/i'     =>  'Safari',
+        '/chrome/i'     =>  'Chrome',
+        '/edge/i'       =>  'Edge',
+        '/opera/i'      =>  'Opera',
+        '/netscape/i'   =>  'Netscape',
+        '/maxthon/i'    =>  'Maxthon',
+        '/konqueror/i'  =>  'Konqueror',
+        '/mobile/i'     =>  'Handheld Browser'
+    );
+
+    foreach ( $browser_array as $regex => $match ) {
+        if ( preg_match( $regex, $userbrowsername ) ) {
+            $browser = $match;
+        }
+    }
+    return $browser;
+}
+
+
+function getOS($userOsName) {
+
+
+    $os_platform =   " ";
+    $os_array =   array(
+        '/windows nt 10/i'      =>  'Win 10 ou 11',
+        '/windows nt 6.3/i'     =>  'Windows 8.1',
+        '/windows nt 6.2/i'     =>  'Windows 8',
+        '/windows nt 6.1/i'     =>  'Windows 7',
+        '/windows nt 6.0/i'     =>  'Windows Vista',
+        '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+        '/windows nt 5.1/i'     =>  'Windows XP',
+        '/windows xp/i'         =>  'Windows XP',
+        '/windows nt 5.0/i'     =>  'Windows 2000',
+        '/windows me/i'         =>  'Windows ME',
+        '/win98/i'              =>  'Windows 98',
+        '/win95/i'              =>  'Windows 95',
+        '/win16/i'              =>  'Windows 3.11',
+        '/macintosh|mac os x/i' =>  'Mac OS X',
+        '/mac_powerpc/i'        =>  'Mac OS 9',
+        '/linux/i'              =>  'Linux',
+        '/ubuntu/i'             =>  'Ubuntu',
+        '/iphone/i'             =>  'iPhone',
+        '/ipod/i'               =>  'iPod',
+        '/ipad/i'               =>  'iPad',
+        '/android/i'            =>  'Android',
+        '/blackberry/i'         =>  'BlackBerry',
+        '/webos/i'              =>  'Mobile'
+    );
+
+    foreach ( $os_array as $regex => $value ) {
+        if ( preg_match($regex, $userOsName ) ) {
+            $os_platform = $value;
+        }
+    }
+    return $os_platform;
+}
+
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/login.css">
+</head>
+<body>
+
+<div class="grid align__item">
+
+    <div class="login">
+
+
+        <h2 style="text-align: center">Please login into your account to take the quiz</h2>
+
+        <form action="./Controller/user.php" method="post" class="form">
+
+            <div class="form__field">
+                <input style="display: none" name="functionName"  type="text" value="checkUser">
+                <input type="text" name="username" placeholder="test@mailaddress.com">
+            </div>
+
+            <div class="form__field">
+                <input type="password" name="password" placeholder="••••••••••••">
+            </div>
+
+            <div class="form__field">
+                <input type="submit" value="Login">
+            </div>
+
+        </form>
+
+
+    </div>
+
+</div></body>
+</html>
